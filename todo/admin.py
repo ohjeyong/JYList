f6f77d419a63f9547fec5061cd72ed46f4d2a947
JeyongOh <jeyong.oh@oshare.kr>
2018-03-25 20:11:30 +0900

    add autocomplete widget on admin

--- todo/admin.py
+++ todo/admin.py
@@ -3,6 +3,19 @@ from django.contrib import admin
 from .models import Todo, TodoComment, Tag
 
 
-admin.site.register(Todo)
-admin.site.register(TodoComment)
-admin.site.register(Tag)
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    search_fields = ['content']
+    autocomplete_fields = ['tag', 'author', 'complete_by']
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    autocomplete_fields = ['author']
+
+
+@admin.register(TodoComment)
+class TodoCommentAdmin(admin.ModelAdmin):
+    search_fields = ['content']
+    autocomplete_fields = ['author']
