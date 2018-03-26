bcc753ac8e40a4d481d4de5c7537c4314445bff8
JeyongOh <jeyong.oh@oshare.kr>
2018-03-26 22:27:38 +0900

    rest framework package use only Token auth as authentication classes

--- config/settings/base.py
+++ config/settings/base.py
@@ -197,8 +197,6 @@ AUTH_USER_MODEL = 'user.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication'
+        'rest_framework.authentication.TokenAuthentication',
     )
 }
