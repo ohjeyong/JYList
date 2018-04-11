2451380e9f8f5c6f250d0515666324e7f1ecd2e4
JeyongOh <jeyong.oh@oshare.kr>
2018-04-12 03:18:49 +0900

    MyPage, Index router

--- front-end/src/components/todo/TodoIndex.tsx
+++ front-end/src/components/todo/TodoIndex.tsx
@@ -1,10 +1,22 @@
 import * as React from 'react';
 import { MainSwitch } from '../../routers/MainSwitch';
+import IconButton from 'material-ui/IconButton';
+import PersonIcon from '@material-ui/icons/Person';
+import { Link } from 'react-router-dom';
 
 export const TodoIndex: React.SFC<{}> = () => (
     <div className="Todo">
         <div className="Header">
-            JY List
+            <Link to="/">
+                <span>JY List</span>
+            </Link>
+            <div>
+                <Link to="/mypage">
+                    <IconButton>
+                        <PersonIcon style={{color: 'white'}}/>
+                    </IconButton>
+                </Link>
+            </div>
         </div>
         <MainSwitch/>
     </div>
--- front-end/src/routers/MainSwitch.tsx
+++ front-end/src/routers/MainSwitch.tsx
@@ -1,15 +1,13 @@
 import * as React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import { TodoList } from '../components/todo/TodoList';
 import { MyPage } from '../components/user/MyPage';
 import { NotFound404 } from '../components/NotFound404';
 
 export const MainSwitch: React.SFC<{}> = () => (
-    <Router>
-        <Switch>
-            <Route exact={true} path="/" component={TodoList}/>
-            <Route exact={true} path="/mypage" component={MyPage}/>
-            <Route path="/" component={NotFound404} />
-        </Switch>
-    </Router>
+    <Switch>
+        <Route exact={true} path="/" component={TodoList}/>
+        <Route exact={true} path="/mypage" component={MyPage}/>
+        <Route path="/" component={NotFound404} />
+    </Switch>
 );
