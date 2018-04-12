347091773030aee71adc569547940d7554faa6a8
JeyongOh <jeyong.oh@oshare.kr>
2018-04-12 17:17:28 +0900

    MyPage 완성

--- front-end/src/components/user/MyPage.tsx
+++ front-end/src/components/user/MyPage.tsx
@@ -1,10 +1,33 @@
 import * as React from 'react';
 import { MyPage as Props } from '../../containers/user/MyPageContainer';
+import { User } from '../../models/user';
+import Avator from 'material-ui/Avatar';
+import PersonIcon from '@material-ui/icons/Person';
+import Button from 'material-ui/Button';
 
 export const MyPage: React.SFC<Props> = (props: Props) => {
+    const loginUser = props.loginUser as User;
     return (
         <div className="MyPage">
-            MyPage
+            <div className="MyPageInner">
+                <div className="MyPageContent">
+                    <Avator>
+                        <PersonIcon/>
+                    </Avator>
+                    <div>
+                        <div>{loginUser.username}</div>
+                        <div>{loginUser.name}</div>
+                    </div>
+                </div>
+                <div>
+                    <Button
+                        variant="raised"
+                        onClick={() => props.logoutRequest()}
+                    >
+                        로그아웃
+                    </Button>
+                </div>
+            </div>
         </div>
     );
 };
