8cbc2a085b1deefdf3760f4ff36c2d14e8f2c3cb
JeyongOh <jeyong.oh@oshare.kr>
2018-04-13 17:56:23 +0900

    Add "nplusone" package

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,11 +1,18 @@
+import logging
+
 from .base import *
 
 DEBUG = True
 ALLOWED_HOSTS = ['localhost']
 
+INSTALLED_APPS.append('nplusone.ext.django')
 
 # insert 2? -> corsheaders middleware must be placed higher than any middleware that can generate responses.
 MIDDLEWARE.insert(2, 'corsheaders.middleware.CorsMiddleware')
+MIDDLEWARE.append('nplusone.ext.django.NPlusOneMiddleware')
+
+NPLUSONE_LOGGER = logging.getLogger('nplusone')
+NPLUSONE_LOG_LEVEL = logging.WARN
 
 CORS_ORIGIN_WHITELIST = (
     'localhost:3000',
